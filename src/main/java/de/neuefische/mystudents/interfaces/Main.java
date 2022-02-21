b31a0e68226a8e9147ececde90043605cf223821
Tanja Kutzner <t.kutzner@arcor.de>
2022-02-21 21:50:14 +0100

    added method to test in main

--- src/main/java/de/neuefische/mystudents/interfaces/Main.java
+++ src/main/java/de/neuefische/mystudents/interfaces/Main.java
@@ -6,8 +6,16 @@ public class Main {
 
         GeschichtsStudent gs = new GeschichtsStudent();
         InformatikStudent is = new InformatikStudent();
+        Student student = new GeschichtsStudent();
 
         System.out.println(gs.getCourse());
         System.out.println(is.getCourse());
+        System.out.println(student.getCourse());
+
+        System.out.println(course(is));
+    }
+
+    public static String course(Student student) {
+        return student.getCourse();
     }
 }
